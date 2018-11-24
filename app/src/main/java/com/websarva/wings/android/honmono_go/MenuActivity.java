42a5e10ede2dc1a7de724d09f2f3a80ef3001ce4
Dntuser <hoshino@flexsystems-inc.com>
2018-11-24 23:46:36 +0900

    現在地から遷移画面をGoogleMapsActivityに変更(現在地まで出る)

--- app/src/main/java/com/websarva/wings/android/honmono_go/MenuActivity.java
+++ app/src/main/java/com/websarva/wings/android/honmono_go/MenuActivity.java
@@ -8,9 +8,11 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -20,6 +22,7 @@ public class MenuActivity extends AppCompatActivity {
     private double latitude = 0;
     //経度フィールド
     private double longitude = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,51 +79,13 @@ public class MenuActivity extends AppCompatActivity {
         }
     }
 
-
-/*
-
-//SKCircle円を表示する機能を取得
-    @Override
-    public void onSingleTap(SKScreenPointt skScreenPoint) {
-        SKCircle skCircle = getSKCircle(skScreenPoint);
-        mapView.addCircle(skCircle);
-    }
-
-    private SKCircle getSKCircle(SKScreenPoint skScreenPoint) {
-        SKCircle skCircle = new SKCircle();
-        //IDを設定する
-        skCircle.setIdentifier(1);
-        //中心点を設定する
-        skCircle.setCircleCenter(mapView.pointToCoordinate(skScreenPoint));
-        //半径を設定する.単位はメートルです。
-        skCircle.setRadius(100f);
-        //外周円の色を設定する
-        skCircle.setOutlineColor(new float[] { 1f, 0f, 0f, 1f });
-        //円内の色を設定する
-        skCircle.setColor(new float[] { 1f, 1f, 1f, 1f });
-        //円周を破線表示にし、間隔の長さを指定する。
-        skCircle.setOutlineDottedPixelsSkip(100);
-        //円周を破線表示にし、線の長さを指定する。
-        skCircle.setOutlineDottedPixelsSolid(100);
-        //円の中心部を空白にする、割合で指定する。
-        skCircle.setMaskedObjectScale(0.1f);
-        //調査1000→パーミッションのコード？？
-        skCircle.setNumberOfPoints(1000);
-        return skCircle;
-    }
-
-    */
-
     public void onCurrentButtonClick(View view){
         //フィールドの緯度と経度の値をもとにマップアプリと連携するURL文字列を生成
-        String urlStr = "geo:" + latitude + "," + longitude;
+        //String urlStr = "geo:" + latitude + "," + longitude;
         //URL文字列からURLオブジェクトを生成
-        Uri uri = Uri.parse(urlStr);
+        //Uri uri = Uri.parse(urlStr);
         //インテントオブジェクトを用意
-        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        //Intent intent = new Intent(MenuActivity.this, SearchResultActivity.class);
-        //第二画面に送るデータ作成
-        //intent.putExtra("uri", uri);
+        Intent intent = new Intent(getApplication(), MapsActivity.class);
         //現在地から検索アクティビティを起動
         startActivity(intent);
     }
